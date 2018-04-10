9da67d2c7286c06c067a8173e2c56c280f9fb36a
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-04-10 13:17:28 +0000

    cleardisp funkar

--- display.py
+++ display.py
@@ -7,8 +7,26 @@ def send(addr, mode, data):
     bus.write_i2c_block_data(addr, mode, data)
 
 def init(ADDRESS):
-    init_commands = [0xae, 0xd5, 0x50, 0x20, 0x81, 0x80, 0xa0, 0xa4, 0xa6, 0xad, 0x80, 0xc0, 0xd9, 0x1f, 0xdb, 0x27, 0xaf, 0xb0, 0x00, 0x11]
-    send(ADDRESS, cmd_mod, init_commands)
+    send(ADDRESS, cmd_mod, [0xae])
+    send(ADDRESS, cmd_mod, [0xd5])
+    send(ADDRESS, cmd_mod, [0x50])
+    send(ADDRESS, cmd_mod, [0x20])
+    send(ADDRESS, cmd_mod, [0x81])
+    send(ADDRESS, cmd_mod, [0x80])
+    send(ADDRESS, cmd_mod, [0xa0])
+    send(ADDRESS, cmd_mod, [0xa4])
+    send(ADDRESS, cmd_mod, [0xa6])
+    send(ADDRESS, cmd_mod, [0xad])
+    send(ADDRESS, cmd_mod, [0x80])
+    send(ADDRESS, cmd_mod, [0xc0])
+    send(ADDRESS, cmd_mod, [0xd9])
+    send(ADDRESS, cmd_mod, [0x1f])
+    send(ADDRESS, cmd_mod, [0xdb])
+    send(ADDRESS, cmd_mod, [0x27])
+    send(ADDRESS, cmd_mod, [0xaf])
+    send(ADDRESS, cmd_mod, [0xb0])
+    send(ADDRESS, cmd_mod, [0x00])
+    send(ADDRESS, cmd_mod, [0x11])
 
 def clearDisplay():
     for j in range (0,16):
