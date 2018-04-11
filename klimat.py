20fa081a3793d6b6445eced026cbc0373329378d
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-04-11 16:10:13 +0000

    fixd length

--- klimat.py
+++ klimat.py
@@ -15,16 +15,17 @@ ccs.tempOffset = temp - 25.0
 
 while(1):
     if ccs.available():
-        display.settextpos(0,0)
-        outtext = "Temp: " + str("%.2f" % ccs.calculateTemperature())
-        display.putstring(outtext)
+        temptext = "Temp: " + str("%.2f" % ccs.calculateTemperature() + "C  ")
         if not ccs.readData(): # should be 0 if read successfull
+            cotext = "CO2:  "+  str(ccs.geteCO2()) + " ppm  "
+            tvoctext = "TVOC: " + str(ccs.getTVOC()) + " ppm   "
+            
+            display.settextpos(0,0)
+            display.putstring(temptext)
             display.settextpos(1,0)
-            outtext = "CO2:  "+  str(ccs.geteCO2())
-            display.putstring(outtext)
+            display.putstring(cotext)
             display.settextpos(2,0)
-            outtext = "TVOC: " + str(ccs.getTVOC())
-            display.putstring(outtext)
+            display.putstring(tvoctext)
         else:
             error = 1
             display.putstring("Ccs unavailable")
