0a0f56dac0ec5bc8ea3c6083f508cf96988f55f2
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-04-24 14:30:18 +0000

    ap-controller fungerar som förväntat

--- ap_controller.py
+++ ap_controller.py
@@ -8,12 +8,12 @@ while(1):
     devicelist=bm.getConnectedDevices()
     if devicelist == []:
         if wifipower:
-            subprocess.call(["sudo service hostapd stop",shell=True])
-            subprocess.call(["sudo ifconfig wlan0 down",shell=True])
+            subprocess.call("sudo service hostapd stop",shell=True)
+            subprocess.call("sudo ifconfig wlan0 down",shell=True)
             wifipower = False
     else:
         if not wifipower:
-            subprocess.call(["sudo ifconfig wlan0 up",shell=True])
-            subprocess.call(["sudo service hostapd start",shell=True])
+            subprocess.call("sudo ifconfig wlan0 up",shell=True)
+            subprocess.call("sudo service hostapd start",shell=True)
             wifipower = True
     sleep(1)
