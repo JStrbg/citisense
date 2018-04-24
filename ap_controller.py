90faa569d2290182943eb8871756a33d323a81b0
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-04-24 13:51:24 +0000

    ap_controller.py

--- ap_controller.py
+++ ap_controller.py
@@ -1,4 +1,5 @@
 import subprocess
+from time import sleep
 from pybtooth import BluetoothManager
 bm=BluetoothManager()
 wifipower = True
@@ -6,12 +7,12 @@ while(1):
     devicelist=bm.getConnectedDevices()
     if devicelist == []:
         if wifipower:
-            subprocess.call(["sudo service hostapd stop",shell=True])
-            subprocess.call(["sudo ifdown wlan0",shell=True])
+            subprocess.call("sudo service hostapd stop", shell=True)
+            subprocess.call("sudo ifdown wlan0", shell=True)
             wifipower = False
     else:
         if not wifipower:
-            subprocess.call(["sudo ifup wlan0",shell=True])
-            subprocess.call(["sudo service hostapd start",shell=True])
+            subprocess.call("sudo ifup wlan0", shell=True)
+            subprocess.call("sudo service hostapd start", shell=True)
             wifipower = True
     sleep(1)
