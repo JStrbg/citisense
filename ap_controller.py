5280f3571d39bf771cda9f8dd079c5f7bd212d0c
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-04-24 16:03:48 +0200

    discoverability on

--- ap_controller.py
+++ ap_controller.py
@@ -2,16 +2,17 @@ import subprocess
 from pybtooth import BluetoothManager
 bm=BluetoothManager()
 wifipower = True
+subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
 while(1):
     devicelist=bm.getConnectedDevices()
     if devicelist == []:
         if wifipower:
             subprocess.call(["sudo service hostapd stop",shell=True])
-            subprocess.call(["sudo ifdown wlan0",shell=True])
+            subprocess.call(["sudo ifconfig wlan0 down",shell=True])
             wifipower = False
     else:
         if not wifipower:
-            subprocess.call(["sudo ifup wlan0",shell=True])
+            subprocess.call(["sudo ifconfig wlan0 up",shell=True])
             subprocess.call(["sudo service hostapd start",shell=True])
             wifipower = True
     sleep(1)
