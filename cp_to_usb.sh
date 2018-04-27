0cd593cd2ccae78981ee0315362c80eb535dad66
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-04-27 14:03:44 +0200

    increased marigin and updated variable_names

--- cp_to_usb.sh
+++ cp_to_usb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash.sh
 echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind #power on USB
-sleep 5
+sleep 8
 sudo cp -n /home/pi/citisense/logs/* /media/pi/KINGSTON/
 sudo umount /media/pi/KINGSTON/
 echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind  #power off usb
+sleep 2
--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import os
 import time
-import i2c_devices as display
+import i2c_devices
 import gas_sensor
 import adc
 import math
@@ -9,35 +9,35 @@ from time import sleep
 from datetime import datetime
 
 def initiate():
-    display.init()
+    i2c_devices.init()
     mode = 0x10 #0x10 = 1_sec_meas, 0x00 idle, 0x20 10_sec_meas, 0x30 60_sec_meas
     gas_sensor.init(mode)
-    display.clearDisplay()
+    i2c_devices.clearDisplay()
 
     temp = gas_sensor.calctemp()
     gas_sensor.tempOffset = temp - 25.0
     #gas_sensor.set_environment(21,50)
 def append_log(temp, co, tvoc, regn, mic):
     if os.path.isdir("/home/pi/citisense/logs/"):
-        display.settextpos(12,-2)
-        display.putstring("Logging..")
+        i2c_devices.settextpos(12,-2)
+        i2c_devices.putstring("Logging..")
         try:
             file = open("/home/pi/citisense/logs/data_log.csv", "a")
         except IOError:
-            display.settextpos(7,-1)
-            display.putstring("IO-Err log")
+            i2c_devices.settextpos(7,-1)
+            i2c_devices.putstring("IO-Err log")
             print("IO-Err logger")
             return 2
         if os.stat("/home/pi/citisense/logs/data_log.csv").st_size == 0:
             file.write('Time, Temp, CO2, TVOC, Rain, Noise\n')
         file.write(datetime.now().strftime('%H:%M:%S') + ", " + str("%.2f" % temp) + ", " + str(co) + ", " + str(tvoc) + ", " + str(round(regn,3)) + ", " + str(mic) + "\n")
         file.close()
-        display.settextpos(12,-2)
-        display.putstring("Logged   ")
+        i2c_devices.settextpos(12,-2)
+        i2c_devices.putstring("Logged   ")
     else:
         print("Io error logger")
-        display.settextpos(12,-2)
-        display.putstring("io error logger         ")
+        i2c_devices.settextpos(12,-2)
+        i2c_devices.putstring("io error logger         ")
         sleep(1)
 def update_sensors(Log, Backup):
     (co,tvoc) = gas_sensor.readsensors()
@@ -51,30 +51,30 @@ def update_sensors(Log, Backup):
     regntext = "Regn: " + str(round(regn,4)) + "V "
     regnrawtext = "RegnRaw: " + str(round(regnraw,2)) + "  "
     mictext = "Mic:  " + str(mic) + "   "
-    display.settextpos(0,-2)
-    display.putstring(temptext)
-    display.settextpos(1,-2)
-    display.putstring(cotext)
-    display.settextpos(2,-2)
-    display.putstring(tvoctext)
-    display.settextpos(3,-2)
-    display.putstring(regntext)
-    display.settextpos(4,-2)
-    display.putstring(regnrawtext)
-    display.settextpos(5,-2)
-    display.putstring(mictext)
+    i2c_devices.settextpos(0,-2)
+    i2c_devices.putstring(temptext)
+    i2c_devices.settextpos(1,-2)
+    i2c_devices.putstring(cotext)
+    i2c_devices.settextpos(2,-2)
+    i2c_devices.putstring(tvoctext)
+    i2c_devices.settextpos(3,-2)
+    i2c_devices.putstring(regntext)
+    i2c_devices.settextpos(4,-2)
+    i2c_devices.putstring(regnrawtext)
+    i2c_devices.settextpos(5,-2)
+    i2c_devices.putstring(mictext)
     err = gas_sensor.checkerror()
     if err:
-        display.settextpos(7,-2)
-        display.putstring("sens_gas err: " + err)
+        i2c_devices.settextpos(7,-2)
+        i2c_devices.putstring("sens_gas err: " + err)
     if Log == True:
         append_log(temp, co, tvoc, regn, mic)
     if Backup == True:
-        display.settextpos(9,-2)
-        display.putstring("WRITING TO USB")
+        i2c_devices.settextpos(9,-2)
+        i2c_devices.putstring("WRITING TO USB")
         subprocess.call(['sudo', 'sh', '/home/pi/citisense/camera.sh'])
         subprocess.call(['sudo', 'sh', '/home/pi/citisense/cp_to_usb.sh'])
-        display.putstring("                    ")
+        i2c_devices.putstring("                    ")
 
 initiate()
 i = 0
@@ -87,5 +87,5 @@ while(1):
         #update_sensors(True,True) #usb-backup + pic
         i = 0
 gas_sensor.close_bus()
-display.close_bus()
+i2c_devices.close_bus()
 adc.close_bus()
