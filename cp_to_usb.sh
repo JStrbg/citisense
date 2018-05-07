a5f03d706360c509b74c1ce2df0d7fba089522c0
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-05-07 12:09:35 +0200

    copy now overwrites files

--- cp_to_usb.sh
+++ cp_to_usb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash.sh
 echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind #power on USB
 sleep 8
-sudo cp -n /home/pi/citisense/logs/* /media/pi/KINGSTON/
-sudo umount /media/pi/KINGSTON/
+sudo cp /home/pi/citisense/logs/* /media/pi/KINGSTON/
+#sudo umount /media/pi/KINGSTON/
 echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind  #power off usb
 sleep 2
