fc17208d33ab56e163ff067300d217d621b17609
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-05-09 13:42:40 +0200

    GATT

--- applauncher.sh
+++ applauncher.sh
@@ -5,10 +5,12 @@ sudo python3 /home/pi/citisense/logger.py &
 #sudo python /home/pi/citisense/bluetooth_autoaccept.py &
 #sudo python /home/pi/citisense/bt_transfer_agent.py &
 #sudo python /home/pi/citisense/ap_controller.py &
-sudo python3 /home/pi/citisense/gatt_server.py &
 sudo sh /home/pi/citisense/hub-off.sh &
 sudo hciconfig hci0 leadv 0
+sudo python3 /home/pi/citisense/gatt_server.py &
+sleep 2
 sudo hciconfig hci0 piscan
+sudo hciconfig hci0 leadv 0
 sudo service xrdp restart
 sleep 2
 sudo service xrdp restart
