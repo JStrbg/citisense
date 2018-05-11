0eb3e9da3b17d60513bef88a38149ff3d524e760
Jonathan Strandberg <jonathanstrandberg95@gmail.com>
2018-05-11 17:46:57 +0200

    cleanup

--- pybtooth/__init__.py
+++
@@ -1,8 +0,0 @@
-import constants
-from .device import BluetoothDevice
-from .manager import BluetoothManager
-from .media_player import BluetoothMediaPlayer
-from .signal_handler import BluetoothSignalHandler
-from .signal_handler import BTSignals
-
-__all__ = ["constants", "BluetoothDevice"]
